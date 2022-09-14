0f36d6f82688a7a52c05b6f451585d2600448c43
Maria Wierzbicka <maria.a.wierzbicka@gmail.com>
2022-09-14 17:40:59 +0200

    Add amount widget to product page

--- src/components/features/ProductPage/ProductPage.js
+++ src/components/features/ProductPage/ProductPage.js
@@ -1,8 +1,11 @@
-import { Alert, Button, Col, Container, Form, Image, Row } from 'react-bootstrap';
+import { Alert, Button, Col, Container, Form, Image, Row, InputGroup} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faMinus } from '@fortawesome/free-solid-svg-icons';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getProductById, getRequest, loadProductsRequest} from '../../../redux/productsRedux';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './ProductPage.module.scss';
 import shortid from 'shortid';
 
@@ -13,8 +16,39 @@ const ProductPage = () => {
   const dispatch = useDispatch();
   
   useEffect(() => {
-    dispatch(loadProductsRequest())
+    dispatch(loadProductsRequest());
   }, [dispatch]);
+
+  const [fullPrice, setFullPrice] = useState('');
+  const [amount, setAmount] = useState(1);
+  const [cartItem, setCartItem] = useState({
+    amount: 1,
+    id: id,
+    basePrice: '',
+    optionPrice: '',
+    optionName: '',
+  });
+
+  const updateAmount = ({ target }) => {
+    setCartItem({ ...cartItem, amount: target.value });
+
+  }
+  const updatePrice = ({target}) => {
+    setCartItem({ ...cartItem, basePrice: product.minPrice, optionPrice: target.value, optionName: target.selectedOptions[0].text });
+    setFullPrice(parseInt(product.minPrice) + parseInt(target.value));
+  }
+
+  const addToCart = e => {
+    e.preventDefault();
+    setCartItem({
+      name: product.name,
+      id: id,
+      basePrice: cartItem.basePrice,
+      optionName: cartItem.optionName,
+      amount: amount,
+    })
+    console.log(cartItem);
+  }
 
   if(request.pending) return <h2>Loading...</h2>; 
   else if(request.error) return <Alert color="warning">{request.error}</Alert>;
@@ -27,17 +61,33 @@ const ProductPage = () => {
           <p>{product.description}</p>
         </Col>
         <Col sm={4} xs={9} className="mx-4 mt-2">
-          <h5>Options</h5>
-          <Form.Select className={styles.dropdown}>
-            <option>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+          <h5>Choose type of wood</h5>
+          <Form.Select onChange={updatePrice} className={styles.dropdown} required>
+            {product.options.map(item => 
+              <option key={item.name} value={item.addedPrice}>{item.name}</option>
+            )}
           </Form.Select>
-          <h5>Price: ${product.minPrice}</h5>
+          <Row className="justify-content-center">
+            <Col xs={8}>
+              <InputGroup className="my-3">
+                <Button variant="outline-secondary" id="less" onClick={() => (amount > 1 ? setAmount(amount-1) : setAmount(amount))}>
+                  <FontAwesomeIcon icon={faMinus} className={styles.icon} />
+                </Button>
+                <Form.Control
+                  value={amount}
+                  onChange={updateAmount}
+                  className="text-center"
+                />
+                <Button variant="outline-secondary" id="more" align="end" onClick={() => setAmount(amount+1)}>
+                  <FontAwesomeIcon icon={faPlus} className={styles.icon} />
+                </Button>
+              </InputGroup>
+            </Col>
+          </Row>
+          <h5>Price: ${fullPrice*amount || product.minPrice*amount}</h5>
           <Row className="justify-content-center">
               <Col xs={12}>
-                <Button className={styles.cartBtn}>Add to cart</Button>
+                <Button className={styles.cartBtn} onClick={addToCart} variant="secondary">Add to cart</Button>
               </Col>
           </Row>
         </Col>
